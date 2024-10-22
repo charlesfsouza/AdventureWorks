fbbe5072ee763da156b3524915169195c1306c1c
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-22 22:38:09 +0000

    Finalizada stg_erp__salesorderheaderdetail

---
+++ models/staging/erp/stg_erp__salesorderheaderdetail.sql
@@ -0,0 +1,24 @@
+with
+    fonte_pedido_item as (
+        select *
+        from {{ source('erp_adventureworks', 'salesorderheaderdetail') }}
+     )   
+
+     ,renomeacao as (
+        select
+
+            cast(salesorderdetailid as int)  as pk_pedido_item
+            ,cast(salesorderid as int) as fk_pedido
+            ,cast(productid as int) as fk_produto
+            ,cast(orderqty as int) as qtd_pedido_item
+            ,cast(unitprice as float) as vlr_item
+            ,cast(unitpricediscount as float) as pct_desconto_item
+        
+        from fonte_pedidos_detalhes
+     )
+
+select *
+from renomeacao
+
+
+
