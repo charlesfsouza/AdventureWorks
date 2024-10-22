b78392f8bdea3ae728f4a9cdee210dda33e0380b
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-22 22:43:28 +0000

    .

--- models/staging/erp/stg_erp__salesorderheader.sql
+++ models/staging/erp/stg_erp__salesorderheader.sql
@@ -7,14 +7,14 @@ with
      ,renomeacao as (
         select
 
-            cast(SALESORDERID as int) as pk_pedido
-            ,cast(STATUS as int) as fk_status_pedido
-            ,cast(CUSTOMERID as int) as fk_cliente
-            ,cast(TERRITORYID as int) as fk_regiao
-            ,cast(CREDITCARDID as int) as fk_cartao_credito
-            ,cast(ORDERDATE as date) as fk_dat_pedido
-            ,cast(DUEDATE as date) fk_dat_vencimento
-            ,cast(SHIPDATE as date) fk_dat_envio
+            cast(salesorderid as int) as pk_pedido
+            ,cast(status as int) as fk_status_pedido
+            ,cast(customerid as int) as fk_cliente
+            ,cast(territoryid as int) as fk_regiao
+            ,cast(creditcardid as int) as fk_cartao_credito
+            ,cast(orderdate as date) as fk_dat_pedido
+            ,cast(duedate as date) fk_dat_vencimento
+            ,cast(shipdate as date) fk_dat_envio
             ,1 as qtd_pedido
         
         from fonte_pedidos
