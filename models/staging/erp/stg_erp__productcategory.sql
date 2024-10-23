0194934eea312f543b49255c6123e3d500d7d0da
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-23 21:52:06 +0000

    finalizada stg_erp__productsubcategory

---
+++ models/staging/erp/stg_erp__productcategory.sql
@@ -0,0 +1,20 @@
+with
+    fonte_produtos_categorias as (
+        select *
+        from {{ source('erp_adventureworks', 'productcategory') }}
+     )   
+
+     ,renomeacao as (
+        select
+
+            cast(productcategoryid as int)  as pk_produto_categoria
+            ,cast(name as character) as dsc_produto_categoria
+       
+        from fonte_produtos_categorias
+     )
+
+select *
+from renomeacao
+
+
+
