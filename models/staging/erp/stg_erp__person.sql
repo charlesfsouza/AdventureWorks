fbb50283b435f30f52adb693599498481dfd9b04
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-25 00:41:09 +0000

    correção na stg_erp__person

--- models/staging/erp/stg_erp__person.sql
+++ models/staging/erp/stg_erp__person.sql
@@ -8,7 +8,9 @@ with
         select
 
             cast(businessentityid as int)  as pk_pessoa
-            ,cast(firstname || ' ' || middlename || ' ' || lastname as character) as nom_pessoa
+            ,cast(firstname as character) as nome_primeiro_pessoa
+            ,cast(middlename as character) as nome_meio_pessoa
+            ,cast(lastname as character) as nome_ultimo_pessoa
             ,cast(persontype as character) as sgl_tipo_pessoa           
        
         from fonte_pessoas
