3e18685522c7e0ee40a4a5f96d8cef2ecc8103b6
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-25 21:55:53 +0000

    Correção na int_status_construcao

--- models/intermediate/int_status_construcao.sql
+++ models/intermediate/int_status_construcao.sql
@@ -1,26 +1,26 @@
 with
     status_pedidos as (
-        select distinct fk_status_pedido
+        select distinct fk_status_pedido as fk_status
         from {{ ref('stg_erp__salesorderheader') }}
     )
     ,status_ordem_compra as (
-        select distinct fk_status_ordem_compra
+        select distinct fk_status_ordem_compra as fk_status
         from {{ ref('stg_erp__purchaseorderheader') }}
     )
     ,uniao as (
-        select distinct fk_status_pedido as fk_status from status_pedidos
-        union all select distinct fk_status_ordem_compra as fk_status from status_ordem_compra
+        select fk_status from status_pedidos
+        union all select fk_status from status_ordem_compra
     )
     , status_tratado as (
         select distinct
             fk_status
             ,case 
-                when fk_status_pedido = 1 then 'In process'
-                when fk_status_pedido = 2 then 'Approved'
-                when fk_status_pedido = 3 then 'Backordered'
-                when fk_status_pedido = 4 then 'Rejected'
-                when fk_status_pedido = 5 then 'Shipped'
-                when fk_status_pedido = 6 then 'Cancelled'
+                when fk_status = 1 then 'In process'
+                when fk_status = 2 then 'Approved'
+                when fk_status = 3 then 'Backordered'
+                when fk_status = 4 then 'Rejected'
+                when fk_status = 5 then 'Shipped'
+                when fk_status = 6 then 'Cancelled'
                 else 'Not informed'
             end dsc_status
         from uniao
