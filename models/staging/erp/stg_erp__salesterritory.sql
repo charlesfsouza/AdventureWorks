78640dc26dc30d7bc3b7ed8fa9bbac29649887d1
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-25 01:30:38 +0000

    Finalizado stg_erp__salesterritory

---
+++ models/staging/erp/stg_erp__salesterritory.sql
@@ -0,0 +1,23 @@
+with
+    fonte_territorio_vendas as (
+        select *
+        from {{ source('erp_adventureworks', 'salesterritory') }}
+     )   
+
+     ,renomeacao as (
+        select
+
+            cast(territoryid as int)  as pk_territorio
+            ,cast(countryregioncode as int) as fk_pais
+            ,cast(name as string) as nom_territorio
+            ,cast("group" as string) as nom_continente
+
+       
+        from fonte_territorio_vendas
+     )
+
+select *
+from renomeacao
+
+
+
