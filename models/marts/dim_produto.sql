aecfc15a413c892dabf74adeb5564ea8628d758f
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-10-27 00:47:24 +0000

    AJUSTES NA DIM_PRODUTO

--- models/marts/dim_produto.sql
+++ models/marts/dim_produto.sql
@@ -1,22 +1,48 @@
-with
-    produtos as (
+with 
+    detalhe_pedidos as (
+        select distinct
+            fk_produto
+        from {{ ref('stg_erp__salesorderdetail') }}
+
+    )
+    ,produtos as (
+        select *
+        from {{ ref('stg_erp__product') }}
+    )
+    ,categorias as (
+        select 
+            pk_produto_categoria
+            ,dsc_produto_categoria
+        from {{ ref('stg_erp__productcategory') }}
+    )
+    ,subcategorias as (
+        select 
+            pk_produto_subcategoria
+            ,fk_produto_categoria
+            ,dsc_produto_subcategoria
+        from {{ ref('stg_erp__productsubcategory') }}
+    )
+   /* ,produtos as (
         select
             pk_produto
             ,dsc_produto
             ,dsc_categoria
             ,dsc_sub_categoria
         from {{ ref('int_produtos_preparacao') }}
-    )
-    ,transformado as (
+    ) */
+    ,joined as (
         select
-            pk_produto
-            ,upper(dsc_produto) as dsc_produto
-            ,upper(dsc_categoria) as dsc_categoria
-            ,upper(dsc_sub_categoria) as dsc_sub_categoria
+            {{ dbt_utils.generate_surrogate_key(['detalhe_pedidos.fk_produto']) }} as sk_produto
+            ,nvl(detalhe_pedidos.fk_produto,'-1') as fk_produto
+            ,nvl(upper(produtos.dsc_produto),'NOT INFORMED') as dsc_produto
+            ,nvl(upper(categorias.dsc_produto_categoria),'NOT INFORMED') as dsc_categoria
+            ,nvl(upper(subcategorias.dsc_produto_subcategoria),'NOT INFORMED') as dsc_sub_categoria
         from
-            produtos
+            detalhe_pedidos
+        left join produtos on produtos.pk_produto = detalhe_pedidos.fk_produto
+        left join subcategorias on subcategorias.pk_produto_subcategoria = produtos.fk_produto_subcategoria
+        left join categorias on categorias.pk_produto_categoria = subcategorias.fk_produto_categoria
     )
 
- 
 select *
-from transformado
+from joined
