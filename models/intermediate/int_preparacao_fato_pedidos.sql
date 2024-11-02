fd6e78b4f87526f57087a50c0ffb783509ea927d
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-11-02 13:48:40 +0000

    correção teste int_preparacao_fato_pedidos

--- models/intermediate/int_preparacao_fato_pedidos.sql
+++ models/intermediate/int_preparacao_fato_pedidos.sql
@@ -122,7 +122,7 @@ with
 
     ,final as (
         select
-             {{ dbt_utils.generate_surrogate_key(['pk_pedido','fk_pedido_item','fk_produto','fk_cliente','fk_pedido_motivo'])}} as sk_pedido_item
+             {{ dbt_utils.generate_surrogate_key(['pk_pedido','fk_pedido_item','fk_produto','fk_cliente','fk_pedido_motivo','fk_motivo'])}} as sk_pedido_item
             ,fk_pedido_item as pk_pedido_item
             ,pk_pedido as fk_pedido
             ,fk_produto
