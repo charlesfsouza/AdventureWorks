53d51f5fc3cfefab9e19f0259a409995e64045d8
charles ferreira de souza <cfsouza86@yahoo.com.br>
2024-11-02 12:08:20 +0000

    substituindo nulo por -1 nas fk da fato pedidos

--- models/marts/fct_pedidos.sql
+++ models/marts/fct_pedidos.sql
@@ -4,12 +4,12 @@ with
              sk_pedido_item
             ,pk_pedido_item
             ,fk_pedido
-            ,fk_produto
-            ,fk_status_pedido
-            ,fk_cliente
-            ,fk_endereco_envio
-            ,fk_cartao_credito
-            ,fk_motivo_pedido
+            ,nvl(fk_produto,'-1') as fk_produto
+            ,nlv(fk_status_pedido,'-1') as fk_status_pedido
+            ,nlv(fk_cliente,'-1') as fk_cliente
+            ,nlv(fk_endereco_envio,'-1') as fk_endereco_envio 
+            ,nlv(fk_cartao_credito,'-1') as fk_cartao_credito 
+            ,nlv(fk_motivo_pedido,'-1') as fk_motivo_pedido
             ,fk_dat_pedido
             ,fk_dat_vencimento
             ,fk_dat_envio
